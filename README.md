803ab158832a9785bc7abb4198717acb35d6a465
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-23 19:35:28 +0600

    project setup with vite

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Kanban Board
+# React + Vite
--- functions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function addTwo(num) {
-    num.toUpperCase();
-    return num + 2;
-}
-addTwo(5);
