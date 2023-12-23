c15fe7af6c21d96cb0da252061fa3f9a3d79e803
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-23 20:09:25 +0600

    create navbar component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import Context from "../Context/ThemeContext";
+import darkModeIcon from "../assets/darkmode.svg";
+import lightModeIcon from "../assets/lightmode.svg";
+import Dropdown from "./Dropdown";
+
+function Navbar() {
+  const { theme, setTheme } = useContext(Context);
+  const handleThemeChange = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  return (
+    <section className="bg-light-secondary dark:bg-dark-secondary dark:text-dark-text">
+      <nav>
+        <div className="ml-4 mr-4 p-4 flex flex-row justify-between">
+          <div>
+            <Dropdown />
+          </div>
+          <div onClick={handleThemeChange}>
+            {theme === "light" ? (
+              <img src={darkModeIcon} />
+            ) : (
+              <img src={lightModeIcon} />
+            )}
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
+}
+
+export default Navbar;
