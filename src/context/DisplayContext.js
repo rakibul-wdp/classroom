3f6f9f09e354889c456cab48fb07cc1850544d53
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-11 16:52:41 +0600

    removed previous project

--- src/context/DisplayContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const DisplayContext = React.createContext();
-
-export default DisplayContext;
--- src/hooks/UseAPI.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const UseAPI = async () => {
-  let res = await axios.get(
-    "https://tfyincvdrafxe7ut2ziwuhe5cm0xvsdu.lambda-url.ap-south-1.on.aws/ticketAndUsers"
-  );
-  res = res.data;
-  return res;
-};
-export default UseAPI;
