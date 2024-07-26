a5e634e596ea21e23c1ab9b7fb31c48980b25fad
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-27 02:39:00 +0600

    setup serverjs

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,21 @@
-require('dotenv').config()
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const classroomRoutes = require("./routes/classroomRoutes");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const userRoutes = require('./routes/user')
+dotenv.config();
+connectDB();
 
-// express app
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-// middleware
-app.use(express.json())
+app.use("/api/auth", authRoutes);
+app.use("/api/classrooms", classroomRoutes);
+app.use("/api/users", userRoutes);
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
-
-// routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
-
-// connect to db
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
