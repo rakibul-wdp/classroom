ac826528ea25a326493c3a33376876e891c90a38
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-03 01:39:00 +0600

    create teacher controller

---
+++ backend/controllers/teacherController.js
@@ -0,0 +1,35 @@
+const Teacher = require("../models/Teacher.js");
+
+const showAllTeachers = async (req, res) => {
+  const data = await Teacher.find({});
+
+  res.json(data);
+};
+
+const createATeacher = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  await Teacher.create({ name, email, password });
+};
+
+const updateATeacher = async (req, res) => {
+  const { name, email, password, className } = req.body;
+
+  await Teacher.findOneAndUpdate(
+    { name },
+    { $set: { name, classroomAssigned: className, email, password } }
+  );
+};
+
+const deleteATeacher = async (req, res) => {
+  const { name } = req.body;
+
+  await Teacher.findOneAndDelete({ name });
+};
+
+module.export = {
+  showAllTeachers,
+  createATeacher,
+  deleteATeacher,
+  updateATeacher,
+};
